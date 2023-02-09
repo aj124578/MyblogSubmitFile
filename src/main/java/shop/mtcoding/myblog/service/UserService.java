1ae89ac8ff7a84bee865ad913fd8e14f2d15a32a
aj124578 <aj124578@naver.com>
2023-02-09 15:53:09 +0900

    UserReq dto작성 완료, service 회원가입 메서드 틀 만들기

---
+++ src/main/java/shop/mtcoding/myblog/service/UserService.java
@@ -0,0 +1,9 @@
+package shop.mtcoding.myblog.service;
+
+import shop.mtcoding.myblog.dto.user.UserReq.JoinReqDto;
+
+public class UserService {
+    public int 회원가입(JoinReqDto joinReqDto){
+        return -1;
+    }
+}
