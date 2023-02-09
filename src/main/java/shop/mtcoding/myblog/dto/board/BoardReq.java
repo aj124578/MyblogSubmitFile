569ec0e004e283c1ce195dd5bd2b4590782eb45e
aj124578 <aj124578@naver.com>
2023-02-09 18:28:39 +0900

    BoardController save 작성 완료

--- src/main/java/shop/mtcoding/myblog/dto/board/BoardReq.java
+++ src/main/java/shop/mtcoding/myblog/dto/board/BoardReq.java
@@ -1,7 +1,12 @@
 package shop.mtcoding.myblog.dto.board;
 
+import lombok.Getter;
+import lombok.Setter;
+
 public class BoardReq {
     
+    @Setter
+    @Getter
     public static  class BoardSaveReqDto{
         private String title;
         private String content;
---
+++ src/main/java/shop/mtcoding/myblog/service/BoardService.java
@@ -0,0 +1,8 @@
+package shop.mtcoding.myblog.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class BoardService {
+    
+}
