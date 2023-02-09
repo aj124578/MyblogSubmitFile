d9615b4e26ae36dc38bb26eb694d3634f1dcd870
aj124578 <aj124578@naver.com>
2023-02-09 18:16:34 +0900

    BoardsaveReq dto 작성완료

---
+++ src/main/java/shop/mtcoding/myblog/dto/board/BoardReq.java
@@ -0,0 +1,9 @@
+package shop.mtcoding.myblog.dto.board;
+
+public class BoardReq {
+    
+    public static  class BoardSaveReqDto{
+        private String title;
+        private String content;
+    }
+}
